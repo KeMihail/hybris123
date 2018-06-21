2690ec36c8edb1d03a08ec38452e03913c012801
LAPTOP-NVKCERU6\Админ <mihaila4038@gmail.com>
2018-06-21 22:08:09 +0300

    servise integration test

--- concerttours/testsrc/concerttours/service/impl/BandServiceIntegrationTest.java
+++ concerttours/testsrc/concerttours/service/impl/BandServiceIntegrationTest.java
@@ -1,10 +1,19 @@
 package concerttours.service.impl;
 
 import de.hybris.bootstrap.annotations.IntegrationTest;
+import de.hybris.platform.impex.jalo.ImpExException;
 import de.hybris.platform.servicelayer.ServicelayerTransactionalTest;
 import de.hybris.platform.servicelayer.model.ModelService;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import concerttours.model.BandModel;
 
 
 @IntegrationTest
@@ -13,4 +22,41 @@ public class BandServiceIntegrationTest extends ServicelayerTransactionalTest
 
 	@Resource
 	private ModelService modelService;
+	@Resource
+	private BandServiceImpl bandServiceImpl;
+
+	private BandModel bandModel;
+
+	private final String code = "code";
+	private final String name = "name";
+	private final String history = "history";
+	private final Long albumSales = 20L;
+
+	@Before
+	public void setUp() throws ImpExException
+	{
+		//importCsv("df.impex", "UTF-8");
+		bandModel = modelService.create(BandModel.class);
+		bandModel.setCode(code);
+		bandModel.setHistory(history);
+		bandModel.setAlbumSales(albumSales);
+	}
+
+	@Test
+	public void testBandService()
+	{
+
+		List<BandModel> bands = bandServiceImpl.getAllBands();
+		final Integer size = bands.size();
+		modelService.save(bandModel);
+
+		bands = bandServiceImpl.getAllBands();
+
+		Assert.assertEquals(size + 1, bands.size());
+		Assert.assertTrue(bands.contains(bandModel));
+
+		final BandModel band = bandServiceImpl.getBandForCode(code);
+		Assert.assertNotNull(band);
+		Assert.assertEquals(code, band.getCode());
+	}
 }
