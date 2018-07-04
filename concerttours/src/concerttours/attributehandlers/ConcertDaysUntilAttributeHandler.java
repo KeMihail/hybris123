ebfbd1a039abb63d7d17fe6172bcfb3be6f46ff7
LAPTOP-NVKCERU6\Админ <mihaila4038@gmail.com>
2018-07-04 21:53:03 +0300

    attributeHandler

---
+++ concerttours/src/concerttours/attributehandlers/ConcertDaysUntilAttributeHandler.java
@@ -0,0 +1,41 @@
+package concerttours.attributehandlers;
+
+import de.hybris.platform.servicelayer.model.attribute.DynamicAttributeHandler;
+
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import concerttours.model.ConcertModel;
+
+
+public class ConcertDaysUntilAttributeHandler implements DynamicAttributeHandler<Long, ConcertModel>
+{
+
+	@Override
+	public Long get(final ConcertModel model)
+	{
+		if (model.getDate() == null)
+		{
+			return null;
+		}
+
+		final ZonedDateTime concertDate = model.getDate().toInstant().atZone(ZoneId.systemDefault());
+		final ZonedDateTime currentDate = ZonedDateTime.now(ZoneId.systemDefault());
+
+		if (concertDate.isBefore(currentDate))
+		{
+			return Long.valueOf(0L);
+		}
+		final Duration duration = Duration.between(concertDate, currentDate);
+
+		return Long.valueOf(duration.toDays());
+	}
+
+	@Override
+	public void set(final ConcertModel model, final Long value)
+	{
+
+	}
+
+}
